d503eb43ffa29136fbf72ba0494aa4b01328b01b
Арсен Исмаилов <ismailovsenya@gmail.com>
2023-07-28 14:23:14 +0600

    задание 47

---
+++ задание 47/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 47. Задайте двумерный массив размером m × n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+  using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int m = 3;
+        int n = 4;
+        double[,] array = new double[m, n];
+
+        Random random = new Random();
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                array[i, j] = random.NextDouble() * (10.0) - 5.0; // Генерация случайного числа от -5.0 до 5.0
+            }
+        }
+
+        // Вывод массива на экран
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+ 
