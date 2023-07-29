78cf7a01ce73d3aed84cae5f8f5f8696fbbf7c9e
Ars <ismailovsenya@gmail.com>
2023-07-30 00:31:19 +0600

    задание 56

---
+++ задание 56/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку
+// с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает суму элементов в каждой стрке и выдаёт номер стрки с наименшей суммой элементов: 1 строка
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int rows = 4;
+        int cols = 4;
+        int[,] array = new int[rows, cols];
+
+        Random random = new Random();
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                array[i, j] = random.Next(1, 11);
+            }
+        }
+
+        Console.WriteLine("Исходный массив:");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        int minRow = 0;
+        int minSum = int.MaxValue;
+
+        for (int i = 0; i < rows; i++)
+        {
+            int rowSum = 0;
+            for (int j = 0; j < cols; j++)
+            {
+                rowSum += array[i, j];
+            }
+
+            if (rowSum < minSum)
+            {
+                minSum = rowSum;
+                minRow = i;
+            }
+        }
+
+        Console.WriteLine($"Строка с наименьшей суммой элементов: {minRow + 1} строка");
+    }
+}
